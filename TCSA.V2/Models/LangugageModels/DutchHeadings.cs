3396689f0dc4cb065a980f18f76470f51c553f72
Stevie Robinson <stevie.robinson@gmail.com>
2024-04-19 20:39:38 +0200

    Add Dutch 224-add-dutch-projects-headings

---
+++ TCSA.V2/Models/LangugageModels/DutchHeadings.cs
@@ -0,0 +1,21 @@
+﻿namespace TCSA.V2.Models.LangugageModels
+{
+    public record DutchHeadings : ILanguageHeadings
+    {
+        public string Introduction => "Inleiding";
+
+        public string Requirements => "Vereisten";
+
+        public string Learn => "Leren";
+
+        public string Resources => "Hulpbronnen";
+
+        public string Tips => "Wat je gaat leren";
+
+        public string Challenges => "Uitdagingen";
+
+        public string RoadmapStepCompleted => "Je hebt deze stap voltooid.";
+
+        public string PendingReview => "Dit project wacht op beoordeling";
+    }
+}

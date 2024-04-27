e8d315bc183b1d6769176ca9b43ef7ca9b570d9b
Dejmenek <dejmen14567@gmail.com>
2024-04-27 22:16:27 +0200

    Add Polish headings

---
+++ TCSA.V2/Models/LangugageModels/PolishHeadings.cs
@@ -0,0 +1,22 @@
+﻿namespace TCSA.V2.Models.LangugageModels;
+
+public record PolishHeadings : ILanguageHeadings
+{
+    public string Introduction => "Wprowadzenie";
+
+    public string Requirements => "Wymagania";
+
+    public string Learn => "Nauka";
+
+    public string Resources => "Zasoby";
+
+    public string Tips => "Czego się nauczysz?";
+
+    public string Challenges => "Wyzwania";
+
+    public string RoadmapStepCompleted => "Ukończyłeś ten etap mapy drogowej.";
+
+    public string PendingReview => "Ten projekt czeka na zatwierdzenie";
+
+    public string ReviewRepository => "Dokonaj przeglądu repozytorium";
+}

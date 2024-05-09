198c3990a5acd70be0f55a92616aca2cd9a91d97
Pablo <thecsharpacademy@gmail.com>
2024-05-09 15:11:58 +1000

    RoadmapHelper

---
+++ TCSA.V2/Helpers/RoadmapHelper.cs
@@ -0,0 +1,70 @@
+﻿namespace TCSA.V2.Helpers;
+
+public static class RoadmapHelper
+{
+    public static readonly List<string> GreenBeltReqs = new()
+    {
+        "Read the 5 articles below",
+        "Complete Freecodecamp C# Certification",
+        "Complete Math Game project"
+    };
+
+    public static readonly List<string> OliveGreenBeltReqs = new() 
+    {
+        "Complete Calculator project",
+        "Complete Habit Logger project",
+        "Complete Coding Tracker project"
+    };
+
+    public static readonly List<string> YellowBeltReqs = new() 
+    {
+        "Complete Flashcards project",
+        "Complete Drinks Info project",
+        "Complete Phone Book project",
+        "Complete Shifts Logger project"
+    };
+    public static readonly List<string> OrangeBeltReqs = new()
+    {
+        "Complete Exercise Tracker project",
+        "Complete Sports Results notifier project",
+        "Complete Excel Reader project",
+        "Complete Unit Testing project"
+    };
+    public static readonly List<string> RedBeltReqs = new()
+    {
+        "Complete Portfolio project",
+        "Complete Resume project",
+        "Complete 2 Code Reviews",
+        "Complete 1 Community Project Issue"
+    };
+    public static readonly List<string> PurpleBeltReqs = new()
+    {
+        "Complete 1 Full-stack area",
+        "Complete 2 Code Reviews",
+        "Complete 1 Community Project Issue"
+    };
+    public static readonly List<string> BrownBeltReqs = new()
+    {
+        "Complete Auth Area",
+        "Complete 3 Code Reviews",
+        "Complete 2 Community Project Issues"
+    };
+    public static readonly List<string> GreyBeltReqs = new()
+    {
+        "Complete SQL Challenges",
+        "Complete 5 Code Reviews",
+        "Complete 3 Community Project Issues"
+    };
+    public static readonly List<string> BlueBeltReqs = new()
+    {
+        "Complete Azure Area",
+        "Complete 5 Code Reviews",
+        "Complete 3 Community Project Issues"
+    };
+    public static readonly List<string> BlackBeltReqs = new()
+    {
+        "Complete at least 2 full-stack areas. One of them has to be MVC, Blazor or Maui",
+        "Complete 8 Code Reviews",
+        "Complete 5 Community Project Issues"
+    };
+}

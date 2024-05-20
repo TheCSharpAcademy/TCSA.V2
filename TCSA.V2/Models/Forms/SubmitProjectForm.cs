c8f4eb8953b990e80215f6618e1156e3a59c1165
Pablo <thecsharpacademy@gmail.com>
2024-05-20 21:21:41 +1000

    Added validation to project submission

---
+++ TCSA.V2/Models/Forms/SubmitProjectForm.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCSA.V2.Models.Forms;
+
+public class SubmitProjectForm
+{
+    public int ProjectId { get; set; }
+    [Required]
+    [GitHubUrl(ErrorMessage = "Invalid GitHub URL. Urls need to link to a valid pull request. They need to start with 'https://github.com/TheCSharpAcademy/CodeReviews'. Example: 'https://github.com/TheCSharpAcademy/CodeReviews.Console.MathGame/pull/42'. Please check how to submit pull requests by clicking on the link in the yellow box above.")]
+    public string GithubUrl { get; set; }
+}
+
+public class GitHubUrlAttribute : ValidationAttribute
+{
+    private const string RequiredUrl = "https://github.com/TheCSharpAcademy/CodeReviews";
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var project = (SubmitProjectForm)validationContext.ObjectInstance;
+
+        if (project.ProjectId == 22 || project.ProjectId == 23)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (value is string githubUrl && !IsValidGitHubUrl(githubUrl))
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+
+        // Default behavior if project ID doesn't match or validation passes
+        return ValidationResult.Success;
+    }
+
+    private bool IsValidGitHubUrl(string url)
+    {
+        // Implement your GitHub URL validation logic here
+        // Example: Check if the URL starts with a specific pattern
+        return url.StartsWith("https://github.com/TheCSharpAcademy/CodeReviews");
+    }
+}
+

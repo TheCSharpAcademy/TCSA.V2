ae75f818409a68a5274b75420d0d17d3cd1ff998
Pablo <thecsharpacademy@gmail.com>
2024-05-29 17:15:21 +1000

    Revamp options config

---
+++ TCSA.V2/Helpers/LinkProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace TCSA.V2.Helpers;
+
+public static class ServiceProviderAccessor
+{
+    public static IServiceProvider ServiceProvider { get; set; }
+}
+
+public class LinkProviderOptions
+{
+    public string DiscordLink { get; set; }
+}
+

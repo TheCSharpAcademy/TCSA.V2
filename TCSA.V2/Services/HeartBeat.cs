b619e5666a448f99ca150075f1e507074c62baac
SERDAR SİPAHİOĞLU <94632226+Doc415@users.noreply.github.com>
2024-06-05 09:37:42 +0300

    Sending a ping to server side to keep connection alive

---
+++ TCSA.V2/Services/HeartBeat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Runtime.CompilerServices;
+
+namespace TCSA.V2.Services;
+
+public class HeartBeat
+{
+    public async Task<string> Beat()
+    {
+        return await Task.FromResult("Stay alive");
+    }
+}

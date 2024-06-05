c0bb26ba04e6afbd8604b49ddfaf5f7b2cc6cee0
SERDAR SİPAHİOĞLU <94632226+Doc415@users.noreply.github.com>
2024-06-05 09:49:57 +0300

    white space

--- TCSA.V2/Services/HeartBeat.cs
+++ TCSA.V2/Services/HeartBeat.cs
@@ -6,7 +6,7 @@ namespace TCSA.V2.Services;
 public class HeartBeat
 {
     //This is a service to ping back to layout page to keep connection alive in intervals
-    //This methos is called from layout page
+    //This method is called from layout page
     public async Task<string> Beat()
     {
         return await Task.FromResult("Stay alive");

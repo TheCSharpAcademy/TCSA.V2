0d086eb7b1c3e86a09b4b4c87816d525818a8971
Pablo <thecsharpacademy@gmail.com>
2024-06-15 17:58:31 +1000

    Transferred methods from UserActivity Component to helper

--- TCSA.V2/Helpers/UserActivityHelper.cs
+++ TCSA.V2/Helpers/UserActivityHelper.cs
@@ -15,4 +15,33 @@ public static class UserActivityHelper
             ActivityType.IssueSubmitted => "issue-submitted"
         };
     }
+
+    public static string GetDescription(List<int> issuesIds, AppUserActivity item, CommunityIssue? issue = null)
+    {
+        switch (item.ActivityType)
+        {
+            case ActivityType.ArticleRead:
+                return $"You read the article <b>{ArticleHelper.GetArticles().Single(x => x.Id == item.ProjectId).Title}</b>";
+            case ActivityType.ProjectSubmitted:
+                return $"You submitted the project <b>{ProjectHelper.GetProjects().Single(x => x.Id == item.ProjectId).Title}</b> for review.";
+            case ActivityType.IssueSubmitted:
+                return $"You submitted the issue <b>{issue.Title}</b> for review.";
+            case ActivityType.ProjectCompleted:
+                return GetTitle(issuesIds, item, issue);
+            case ActivityType.CodeReviewCompleted:
+                return $"You've finished a code review for a <b>{ProjectHelper.GetProjects().Single(x => x.Id == item.ProjectId).Title}</b> project.";
+            default:
+                return "";
+        }
+    }
+
+    public static string GetTitle(List<int> issuesIds, AppUserActivity item, CommunityIssue? issue = null)
+    {
+        if (issuesIds.Contains(item.ProjectId))
+        {
+            return $"The open source ticket <b>{issue.Title}</b> was marked as complete.";
+        }
+
+        return $"The project <b>{ProjectHelper.GetProjects().Single(x => x.Id == item.ProjectId).Title}</b> was marked as complete.";
+    }
 }

a4c79d6ab451cd2d3051e73a968a632b003ae5da
serdar sipahioğlu <sipahioglu_Serdar@yahoo.com>
2024-08-25 13:10:16 +0300

    Added item filter to show

---
+++ TCSA.V2/Migrations/20240825093928_modelupdate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TCSA.V2.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "ShowcaseItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "0e22e5fa-3f59-47af-a6de-ee153057b7d2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "ShowcaseItems");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "e34d0817-cdea-4633-83d7-5744c3728ab9");
+        }
+    }
+}
--- TCSA.V2/Services/GalleryService.cs
+++ TCSA.V2/Services/GalleryService.cs
@@ -1,5 +1,21 @@
-﻿namespace TCSA.V2.Services;
+﻿using TCSA.V2.Models;
+
+namespace TCSA.V2.Services;
 
 public class GalleryService
 {
+
+    public async Task<IEnumerable<ShowcaseItem>> GetItems()
+    {
+
+        return new List<ShowcaseItem>()
+        {
+            new ShowcaseItem()
+            {
+                Id = 1,
+                Area=Area.React,
+                ProjectName="Friends Manager"
+            }
+        };
+    }
 }

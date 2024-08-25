6d1da62097b0c66f84233debac6ac0c01659109d
serdar sipahioğlu <sipahioglu_Serdar@yahoo.com>
2024-08-25 13:10:16 +0300

    Added item filter to show

--- TCSA.V2/Models/ShowcaseItem.cs
+++ TCSA.V2/Models/ShowcaseItem.cs
@@ -9,7 +9,7 @@ public class ShowcaseItem
     public string AppUserId { get; set; }
     public int ProjectId { get; set; }
     public Area Area { get; set; }
-
+    public DateTime DateCreated { get; set; }= DateTime.Now;
     public bool GoldenProject { get; set; } = false;
     public string Link { get; set; }
     public int Likes { get; set; } = 0;

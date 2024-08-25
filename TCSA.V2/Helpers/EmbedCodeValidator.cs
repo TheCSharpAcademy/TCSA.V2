8059e5de70fe0f415998a563ad80cae389aa1c54
serdar sipahioğlu <sipahioglu_Serdar@yahoo.com>
2024-08-25 22:07:56 +0300

    Added youtube embed code validator

---
+++ TCSA.V2/Helpers/EmbedCodeValidator.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace TCSA.V2.Helpers
+{
+    public class EmbedCodeValidator:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Embed code cant be empty");
+            }
+
+            var embedCode = value.ToString();           
+            string pattern = @"<iframe.*?src=""https:\/\/www\.youtube\.com\/embed\/[a-zA-Z0-9_-]+""[^>]*><\/iframe>";
+            Regex regex = new Regex(pattern);
+
+            if (!regex.IsMatch(embedCode))
+            {
+                return new ValidationResult("The embed code is not a valid YouTube embed code.");
+            }
+                      
+            return ValidationResult.Success;
+        }
+    }
+}
+

fa637401cfb81840e605e8b1ce89bf9f1cae29a3
serdar sipahioğlu <sipahioglu_Serdar@yahoo.com>
2024-08-26 13:12:46 +0300

    Gallery navbar added Design update Search per user

--- TCSA.V2/Migrations/20240826072744_Rebase.Designer.cs
+++ TCSA.V2/Migrations/20240826072744_Rebase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TCSA.V2.Data;
 
@@ -11,9 +12,11 @@ using TCSA.V2.Data;
 namespace TCSA.V2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240826072744_Rebase")]
+    partial class Rebase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

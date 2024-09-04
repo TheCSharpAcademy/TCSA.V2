48854668c86a1e1fb85a5e20de401c701e1d0882
Pablo <thecsharpacademy@gmail.com>
2024-09-04 19:48:45 +0100

    Showcase card improvements 2

--- TCSA.V2/Models/DTO/ShowcaseItemDTO.cs
+++ TCSA.V2/Models/DTO/ShowcaseItemDTO.cs
@@ -5,8 +5,7 @@ public class ShowcaseItemDTO
     public string ApplicationUserId { get; set; }
     public int ProjectId { get; set; }
     public string ProjectName { get; set; }
-    public string UserFirstName { get; set; }
-    public string UserLastName { get; set; }
+    public string Username { get; set; }
     public Area Area { get; set; }
     public string VideoUrl { get; set; }
 }

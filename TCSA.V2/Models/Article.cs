04f30e8501b7cf4accfb052073a8a946c3d6ccf5
Pablo <thecsharpacademy@gmail.com>
2024-09-27 05:55:05 +1000

    Articles Page

--- TCSA.V2/Models/Article.cs
+++ TCSA.V2/Models/Article.cs
@@ -18,6 +18,7 @@ public class Article
     public Level Level { get; set; }
     public bool IsClosed { get; set; }
     public string NextSlug { get; set; }
+    public string CardImgUrl { get; set; }  
 }
 
 public class Block

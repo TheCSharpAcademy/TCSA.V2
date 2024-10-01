06c75c14ae8eeb3c20964aa9f9f179e63af547ee
Pablo <thecsharpacademy@gmail.com>
2024-10-02 06:38:11 +1000

    Courses Skeleton

---
+++ TCSA.V2/Models/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace TCSA.V2.Models;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public List<Article> Articles { get; set; }
+}
